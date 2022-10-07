863dcbbd968a6c68afb87a2567f53d689215fd7e
Yogesh Sharma <sharyogi@amazon.com>
2022-10-07 12:53:35 +0000

    Add real psql call for install/uninstall/list

--- tools/pg_tle_manage_local_extension/README.md
+++ tools/pg_tle_manage_local_extension/README.md
@@ -1,5 +1,5 @@
 # pg_tle_manage_local_extension
-A rust app to install a local extension in PostgreSQL database using backcountry (pg_tle) extension. App will call pg_tle.install_extension to install extension. After this DBA/User can execute create extension.
+A bash script to manage local extension in PostgreSQL database using pg_tle extension. Script will call pgtle.<functions> to manage extension.
 
 # Requirements
 
@@ -12,63 +12,73 @@ A rust app to install a local extension in PostgreSQL database using backcountry
 
 # How to run
 
-`pg_tle_manage_local_extension.sh "<pg conect string name=value>" "<path to extension source>" <extension name> <extension version>`
-
-# Example
-
-In this example we will install mv_stats extension from https://gitlab.com/ongresinc/extensions/mv_stats
+In this example we will install mv_stats extension from https://gitlab.com/ongresinc/extensions/mv_stats  
 The mv_stats extension is a means for tracking some statistics of all materialized views in a database.
 
-```
-git clone https://gitlab.com/ongresinc/extensions/mv_stats.git ../mv_stats
-
-cd pg_tle_manage_local_extension
-```
-
-`./pg_tle_manage_local_extension.sh "host=127.0.0.1 dbname=mydb user=myuser port=5414 sslmode=require" ../mv_stats mv_stats 0.2.0 --action install`
-or
-`./pg_tle_manage_local_extension.sh "postgresql://myuser:mypassword@127.0.0.1:5414/mydb?sslmode=require" ../mv_stats mv_stats 0.2.0 --action install`
-
-More details on PostgreSQL connection https://docs.rs/postgres/latest/postgres/config/struct.Config.html
+## Clone mv_stats extension
 
 ```
-select * from pg_tle.available_extensions();
-    name    | default_version |                                comment                                
-------------+-----------------+-----------------------------------------------------------------------
-...
- mv_stats   | 0.2.0           | Extension to track statistics about materialized view in the database
-...
-```
-
-# Usage
-
-`./pg_tle_manage_local_extension --help`
+git clone https://gitlab.com/ongresinc/extensions/mv_stats.git ~/mv_stats
 
 ```
 
+## Get usage / help
+
+`./pg_tle_manage_local_extension.sh --help`
+
+```
 pg_tle_manage_local_extension.sh 1.0
-Install local extension in PostgreSQL using pg_tle
+Manage local extension in PostgreSQL using pg_tle
 
 USAGE:
-    pg_tle_manage_local_extension.sh <OPTIONS>
+
+  Install:
+    pg_tle_manage_local_extension.sh \
+      --action install \
+      --pgconn "postgresql://sharyogi@localhost:5414/postgres?sslmode=prefer" \
+      --extname <ExtensionName> \
+      --extrev <ExtensionVersion> \
+      --extpath <ExtensionPath>
+
+  Remove:
+    pg_tle_manage_local_extension.sh \
+      --action uninstall \
+      --pgconn "postgresql://sharyogi@localhost:5414/postgres?sslmode=prefer" \
+      --extname <ExtensionName>
+
+  List:
+    pg_tle_manage_local_extension.sh \
+      --action list \
+      --pgconn "postgresql://sharyogi@localhost:5414/postgres?sslmode=prefer"
+
+  List Versions along wtih Extensions:
+    pg_tle_manage_local_extension.sh \
+      --action list-versions \
+      --pgconn "postgresql://sharyogi@localhost:5414/postgres?sslmode=prefer"
 
 OPTIONS:
+
+    -a, --action <action name>
+          A required parameter.
+          install - install extension 
+          uninstall - uninstall extension
+          list - list extension
+          list-versions - list extension along with available versions
+
     -c, --pgconn <PG Connection>
+          A required parameter.
           PostgreSQL connection string (URI or "Name=Value Name1=V1" format)
 
-    -p, --extpath <Extension Path>
-          Local path of the extension
-
     -n, --extname <Extension Name>
+          A required parameter for install and uninstall actions.
           Name of the extension
 
     -r, --extrev <Extension Revision>
           Extension revision to install
 
-    -a, --action <action name>
-          install - install extension 
-          update - update extension
-          remove - remove extension
+    -p, --extpath <Extension Path>
+          A required parameter for install action.
+          Local path of the extension
 
     -h, --help
           Print help information
@@ -76,4 +86,76 @@ OPTIONS:
     -V, --version
           Print version information
 
+EXIT_CODES:
+  1 - PostgreSQL command line tool psql is missing
+  2 - Missing argument or Invalid arguments value
+  3 - Extension source code folder (--extpath) not found
+  4 - Error running conencting to PostgreSQL or error executiong SQL query
+
 ```
+
+## Version info
+
+`./pg_tle_manage_local_extension.sh --version`
+
+```
+pg_tle_manage_local_extension.sh 1.0
+```
+
+## Install extension
+
+`./pg_tle_manage_local_extension.sh --action install --pgconn "postgresql://myuser@localhost:5432/mydb?sslmode=prefer" --extname mv_stats --extpath ~/mv_stats`
+
+```
+Installing mv_stats  from ~/mv_stats in postgresql://myuser@localhost:5432/mydb?sslmode=prefer
+
+install compeleted succefully.
+```
+
+or install a specific version
+
+`./pg_tle_manage_local_extension.sh --action install --pgconn "postgresql://myuser@localhost:5432/mydb?sslmode=prefer" --extname mv_stats --extpath ~/mv_stats --extrev 0.2.0`
+
+```
+Installing mv_stats 0.2.0 from ~/mv_stats in postgresql://myuser@localhost:5432/mydb?sslmode=prefer
+
+install compeleted succefully.
+```
+
+## List extension(s)
+
+`./pg_tle_manage_local_extension.sh --action list --pgconn "postgresql://myuser@localhost:5432/mydb?sslmode=prefer"`
+
+```
+List pg_tle installed extension(s) from postgresql://sharyogi@localhost:5414/postgres?sslmode=prefer
+
+   name   | default_version |                                comment                                
+----------+-----------------+-----------------------------------------------------------------------
+ mv_stats | 0.2.0           | Extension to track statistics about materialized view in the database
+(1 row)
+```
+
+## List installed versions of all extensions
+
+`./pg_tle_manage_local_extension.sh --action list-versions --pgconn "postgresql://myuser@localhost:5432/mydb?sslmode=prefer"`
+
+
+```
+List pg_tle installed extension(s) from postgresql://sharyogi@localhost:5414/postgres?sslmode=prefer
+
+   name   | version | superuser | trusted | relocatable | schema | requires |                                comment                                
+----------+---------+-----------+---------+-------------+--------+----------+-----------------------------------------------------------------------
+ mv_stats | 0.2.0   | f         | f       | f           |        | {pg_tle} | Extension to track statistics about materialized view in the database
+(1 row)
+```
+
+## Uninstall extension
+
+`./pg_tle_manage_local_extension.sh --action uninstall --pgconn "postgresql://myuser@localhost:5432/mydb?sslmode=prefer" --extname mv_stats`
+
+```
+Removing mv_stats  from postgresql://myuser@localhost:5432/mydb?sslmode=prefer
+
+uninstall compeleted succefully.
+```
+
--- tools/pg_tle_manage_local_extension/pg_tle_manage_local_extension.sh
+++ tools/pg_tle_manage_local_extension/pg_tle_manage_local_extension.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
+###########################
+#  Schema |              Name              | Result data type |                                                                     Argument data types                                                                      | Type
+# --------+--------------------------------+------------------+--------------------------------------------------------------------------------------------------------------------------------------------------------------+------
+#  pgtle  | available_extension_versions   | SETOF record     | OUT name name, OUT version text, OUT superuser boolean, OUT trusted boolean, OUT relocatable boolean, OUT schema name, OUT requires name[], OUT comment text | func
+#  pgtle  | available_extensions           | SETOF record     | OUT name name, OUT default_version text, OUT comment text                                                                                                    | func
+#  pgtle  | extension_update_paths         | SETOF record     | name name, OUT source text, OUT target text, OUT path text                                                                                                   | func
+#  pgtle  | install_extension              | boolean          | name text, version text, trusted boolean, description text, ext text, requires text[] DEFAULT NULL::text[], encoding text DEFAULT NULL::text                 | func
+#  pgtle  | install_update_path            | boolean          | name text, fromvers text, tovers text, ext text                                                                                                              | func
+#  pgtle  | pg_tle_feature_info_sql_drop   | event_trigger    |                                                                                                                                                              | func
+#  pgtle  | pg_tle_feature_info_sql_insert | void             | proc regproc, feature pgtle.pg_tle_features                                                                                                                  | func
+#  pgtle  | uninstall_extension            | boolean          | extname text                                                                                                                                                 | func
+# (8 rows)
+###########################
+
 PROGRAM_NAME=$(basename $0)
 PROGRAM_VERSION="1.0"
 
 SUPPORTED_ARGS=$(getopt -o c:a:p:n:r:hv --long pgconn:,action:,extpath:,extname:,extrev:,help,version --name ${PROGRAM_NAME} -- "$@")
+
+PGCLI=$(which psql)
+PGFLAGS_DML="--quiet --no-align --tuples-only"
+PGFLAGS_QRY="--quiet"
 
 version() {
   cat <<VERSION_EOF
@@ -17,28 +35,57 @@ usage() {
   cat <<USAGE_EOF
 
 ${PROGRAM_NAME} ${PROGRAM_VERSION}
-Install local extension in PostgreSQL using pg_tle
+Manage local extension in PostgreSQL using pg_tle
 
 USAGE:
-    ${PROGRAM_NAME} <OPTIONS>
+
+  Install:
+    ${PROGRAM_NAME} \\
+      --action install \\
+      --pgconn "postgresql://sharyogi@localhost:5414/postgres?sslmode=prefer" \\
+      --extname <ExtensionName> \\
+      --extrev <ExtensionVersion> \\
+      --extpath <ExtensionPath>
+
+  Remove:
+    ${PROGRAM_NAME} \\
+      --action uninstall \\
+      --pgconn "postgresql://sharyogi@localhost:5414/postgres?sslmode=prefer" \\
+      --extname <ExtensionName>
+
+  List:
+    ${PROGRAM_NAME} \\
+      --action list \\
+      --pgconn "postgresql://sharyogi@localhost:5414/postgres?sslmode=prefer"
+
+  List Versions along wtih Extensions:
+    ${PROGRAM_NAME} \\
+      --action list-versions \\
+      --pgconn "postgresql://sharyogi@localhost:5414/postgres?sslmode=prefer"
 
 OPTIONS:
+
+    -a, --action <action name>
+          A required parameter.
+          install - install extension 
+          uninstall - uninstall extension
+          list - list extension
+          list-versions - list extension along with available versions
+
     -c, --pgconn <PG Connection>
+          A required parameter.
           PostgreSQL connection string (URI or "Name=Value Name1=V1" format)
 
-    -p, --extpath <Extension Path>
-          Local path of the extension
-
     -n, --extname <Extension Name>
+          A required parameter for install and uninstall actions.
           Name of the extension
 
     -r, --extrev <Extension Revision>
           Extension revision to install
 
-    -a, --action <action name>
-          install - install extension 
-          update - update extension
-          remove - remove extension
+    -p, --extpath <Extension Path>
+          A required parameter for install action.
+          Local path of the extension
 
     -h, --help
           Print help information
@@ -46,7 +93,18 @@ OPTIONS:
     -V, --version
           Print version information
 
+EXIT_CODES:
+  1 - PostgreSQL command line tool psql is missing
+  2 - Missing argument or Invalid arguments value
+  3 - Extension source code folder (--extpath) not found
+  4 - Error running conencting to PostgreSQL or error executiong SQL query
+
 USAGE_EOF
+}
+
+RUN_PGSQL(){
+  PG_OUTPUT=$(psql ${pgConn} ${PGFLAGS} --command="${SQL_QUERY}" 2>&1)
+  PG_EXIT=$?
 }
 
 eval set -- "$SUPPORTED_ARGS"
@@ -94,28 +152,116 @@ done
 if [ $# -gt 0 ]; then
   printf "\nExtra argument(s) passed, %s\n" "$@" >&2 
   usage >&2
+  exit 2
+fi
+
+if [ -z "${PGCLI}" ] ; then
+  printf "\nPostgreSQL command line client 'psql' not found in PATH\n" >&2
   exit 1
 fi
-if [ -z "pgConn" ] || [ -z "ExtName" ] || [ -z "ExtRev" ] || [ -z "ExtPath" ] || [ -z "Action" ] ; then
-  printf "\nMissing argument\n" >&2 
+
+if [ -z "${Action}" ] ; then
+  printf "\nMissing argument(s) --action is required\n" >&2 
   usage >&2
-  exit 1
+  exit 2
+fi
+
+if [ -z "${pgConn}" ] ; then
+  printf "\nMissing argument(s) --pgconn is required\n" >&2 
+  usage >&2
+  exit 2
 fi
 
 case "$Action" in
   install)
+    if [ -z "${ExtName}" ] || [ -z "${ExtPath}" ] ; then
+      printf "\nMissing argument(s) --extname --extpath are required for install\n" >&2 
+      usage >&2
+      exit 2
+    fi
+    if [ ! -d ${ExtPath} ] ; then
+      printf "\nFATAL: Extension path %s is missing.\n" ${ExtPath} >&2
+      exit 3
+    fi
+    ExtControl="${ExtPath}/${ExtName}.control"
+    ExtSQL_Template="${ExtPath}/${ExtName}--%s.sql"
+    if [ ! -f "${ExtControl}" ] ; then
+      printf "\nFATAL: Extension control file %s is missing.\n" ${ExtControl} >&2
+      exit 3
+    fi
+    printf -v ExtSQL "${ExtSQL_Template}" "${ExtRev}"
+    ExtensionsList=""
+    if [ ! -z "${ExtRev}" ] ; then
+      if [ ! -f "${ExtSQL}" ] ; then
+        printf "\nFATAL: Extension SQL file %s is missing.\n" ${ExtSQL} >&2
+        exit 3
+      else
+        ExtensionsList="${ExtRev},${ExtSQL}"
+      fi
+    else
+      printf -v ExtSQL "${ExtSQL_Template}" "*"
+      for file in ${ExtSQL}
+      do
+        parsed=$(sed -e "s:.*${ExtName}--::g;s:.sql::g;s:--:,:g; s:$:,${file}:g" <<< ${file} )
+        ExtensionsList="${ExtensionsList} ${parsed}"
+      done
+    fi
     printf "\nInstalling ${ExtName} ${ExtRev} from ${ExtPath} in ${pgConn}\n"
+    PGFLAGS=${PGFLAGS_DML}
+    ControlContent=$(cat ${ExtControl})
+    for revs in $ExtensionsList
+    do
+      IFS=, read -r v1 v2 v3 <<< $revs
+      if [ -z "${v3}" ]; then
+        SUFLAG="$(sed -ne "s/ //g;s/^trusted=\(.*\)/\1/p" < ${ExtControl})"
+        SUFLAG=${SUFLAG:-false}
+        SQL_QUERY="SELECT FROM pgtle.install_extension('${ExtName}', '${v1}', ${SUFLAG}, '"$(sed -ne "s/^comment.*=.*'\(.*\)'/\1/p" < ${ExtControl})"', \$_PG_TLE_SQL_\$$(cat ${v2})\$_PG_TLE_SQL_\$);"
+      else
+        SQL_QUERY="SELECT FROM pgtle.install_update_path('${ExtName}', '${v1}', '${v2}', \$_PG_TLE_SQL_\$$(cat ${v3})\$_PG_TLE_SQL_\$);"
+      fi
+      RUN_PGSQL
+    done
     ;;
-  update)
-    printf "\nUpdating ${ExtName} ${ExtRev} from ${ExtPath} in ${pgConn}\n"
-    ;;
-  remove)
+  uninstall)
+    if [ -z "${ExtName}" ] ; then
+      printf "\nMissing argument(s) --extname is required for uninstall\n" >&2 
+      usage >&2
+      exit 2
+    fi
     printf "\nRemoving ${ExtName} ${ExtRev} from ${pgConn}\n"
+    PGFLAGS=${PGFLAGS_DML}
+    SQL_QUERY="SELECT FROM pgtle.uninstall_extension('${ExtName}');"
+    RUN_PGSQL
+    ;;
+  list|list-versions)
+    printf "\nList pg_tle installed extension(s) from ${pgConn}\n"
+    PGFLAGS=${PGFLAGS_QRY}
+    if [ $Action = "list" ] ; then
+      SQL_QUERY='SELECT * FROM pgtle.available_extensions();'
+    else
+      SQL_QUERY='SELECT * FROM pgtle.available_extension_versions();'
+    fi
+    RUN_PGSQL
     ;;
   *)
     printf "\nInvalid Action requested, %s\n" "${Action}" >&2 
     usage >&2
-    exit 1
+    exit 2
     ;;
 esac
+if [ "${PG_EXIT}" -eq 0 ] ; then
+  if [ $Action == "list" ] || [ $Action == "list-versions" ] ; then
+    if [[ "${PG_OUTPUT}" == *"(0 rows)"* ]] ; then
+      printf "\nNo pg_tle managed extension(s) available in ${pgConn}\n"
+    else
+      printf "\n${PG_OUTPUT}\n"
+    fi
+  else
+    printf "\n$Action compeleted succefully.\n"
+  fi
+  exit 0
+else
+  printf "\nExecution Failed\nExit code (psql): ${PG_EXIT}\nError (psql): ${PG_OUTPUT}\n" >&2
+  exit 4
+fi
 
