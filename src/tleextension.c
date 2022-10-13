3c348e84850110aac1ebfdf97cf961c4150f4738
Jonathan S. Katz <jkatz@amazon.com>
2022-10-13 12:24:24 -0400

    Set extension encoding when client/server encoding differ
    If client/server encoding do not match, go with the client
    encoding so long as it is set to a recognized PostgreSQL encoding.

--- src/tleextension.c
+++ src/tleextension.c
@@ -4181,6 +4181,18 @@ pg_tle_install_extension(PG_FUNCTION_ARGS)
 	control->comment = pstrdup(extdesc);
 	control->requires = reqlist;
 
+	/*
+	 * see if client and server encoding differ. if they do, set the encoding
+	 * explicitly to the client encoding
+	 */
+	if (GetDatabaseEncoding() != pg_get_client_encoding() && pg_get_client_encoding() >= 0)
+	{
+		control->encoding = pg_get_client_encoding();
+		ereport(NOTICE,
+				(errmsg("Extension encoding set to \"%s\".", pg_get_client_encoding_name()),
+				 errhint("To set a different encoding, change the value of \"client_encoding\".")));
+	}
+
 	ctlstr = build_extension_control_file_string(control);
 
 	/*
