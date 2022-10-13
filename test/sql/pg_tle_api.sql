b51567c9f6612bcb4be8cf789bfe75c79f8597d6
Jonathan S. Katz <jkatz@amazon.com>
2022-10-13 10:44:54 -0400

    Fix tests for PostgreSQL 15
    An unprivileged user was trying to create a function without
    CREATE privileges in the public schema. This was adjusted to
    give this user their own schema that they have authorization to
    create functions in. The test ethos itself remains the same.

--- test/sql/pg_tle_api.sql
+++ test/sql/pg_tle_api.sql
@@ -46,11 +46,12 @@ ALTER ROLE testuser with password 'pass';
 ALTER ROLE testuser with password 'passwords';
 -- Test that by default a role has access to the feature_info table
 CREATE ROLE testuser_2 with LOGIN;
+CREATE SCHEMA testuser_2 AUTHORIZATION testuser_2;
 SET SESSION AUTHORIZATION testuser_2;
 ALTER ROLE testuser_2 with password 'pass';
 -- Test that by default unprivileged users do not have permission to insert into table
 -- or have access to functions
-CREATE OR REPLACE FUNCTION unpriv_function_passcheck(username text, shadow_pass text, password_types pgtle.password_types, validuntil_time TimestampTz,validuntil_null boolean) RETURNS void AS
+CREATE OR REPLACE FUNCTION testuser_2.unpriv_function_passcheck(username text, shadow_pass text, password_types pgtle.password_types, validuntil_time TimestampTz,validuntil_null boolean) RETURNS void AS
 $$
 BEGIN
 if length(shadow_pass) < 8 then
@@ -59,10 +60,9 @@ end if;
 END;
 $$
 LANGUAGE PLPGSQL;
-INSERT INTO pgtle.feature_info VALUES ('passcheck', 'public', 'unpriv_function_passcheck', 'public.unpriv_function_passcheck(pg_catalog.text,pg_catalog.text,pgtle.password_types,timestamp with time zone,boolean)');
-SELECT pgtle.register_feature('unpriv_function_passcheck', 'passcheck');
+INSERT INTO pgtle.feature_info VALUES ('passcheck', 'testuser_2', 'unpriv_function_passcheck', 'public.unpriv_function_passcheck(pg_catalog.text,pg_catalog.text,pgtle.password_types,timestamp with time zone,boolean)');
+SELECT pgtle.register_feature('testuser_2.unpriv_function_passcheck', 'passcheck');
 SELECT pgtle.unregister_feature('password_check_length_greater_than_8', 'passcheck');
-INSERT INTO pgtle.feature_info VALUES ('passcheck', '', 'unpriv_function_passcheck', '');
 DELETE FROM pgtle.feature_info where feature = 'passcheck';
 RESET SESSION AUTHORIZATION;
 
@@ -101,7 +101,8 @@ TRUNCATE TABLE pgtle.feature_info;
 INSERT INTO pgtle.feature_info VALUES ('passcheck', 'public', 'test_foo;select foo()', '');
 ALTER ROLE testuser with password '123456789';
 DROP ROLE testuser;
-DROP FUNCTION unpriv_function_passcheck;
+DROP FUNCTION testuser_2.unpriv_function_passcheck;
+DROP SCHEMA testuser_2;
 DROP ROLE testuser_2;
 ALTER SYSTEM RESET pgtle.enable_password_check;
 SELECT pg_reload_conf();
